157c8b3bc25a2db9a2d7e38814a1a286b318dc6f
LokeshInfo <lokesh.gadekar@infocentroidtech.com>
2020-03-20 15:07:21 +0530

    Updates 20 March 2020 :)

--- app/src/main/java/com/icss/rcorporation/MainActivity.java
+++ app/src/main/java/com/icss/rcorporation/MainActivity.java
@@ -2,9 +2,15 @@ package com.icss.rcorporation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.GeolocationPermissions;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -14,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
     ProgressDialog pd;
-    private String r_url = "http://rcorporation.in/software/operator_login";
+    private String r_url = "https://rcorporation.in/software/operator_login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +29,63 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.webview);
 
         WebSettings webSettings = webView.getSettings();
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
 
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setGeolocationEnabled(true);
+        webView.getSettings().setGeolocationDatabasePath( (MainActivity.this).getFilesDir().getPath() );
+
+        // HTML5 API flags
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+
+            @Override
+            public void onGeolocationPermissionsShowPrompt(final String origin,final GeolocationPermissions.Callback callback) {
+                //super.onGeolocationPermissionsShowPrompt(origin, callback);
+                callback.invoke(origin, true, false);
+                Log.i("TAG --- GEOLOCATION", "   onGeolocationPermissionsShowPrompt()");
+
+                final boolean remember = false;
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Locations");
+                builder.setMessage("Would like to use your Current Location ")
+                        .setCancelable(true).setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // origin, allow, remember
+                        callback.invoke(origin, true, remember);
+                    }
+                }).setNegativeButton("Don't Allow", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // origin, allow, remember
+                        callback.invoke(origin, false, remember);
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
+        webView.loadUrl(r_url);
+
+/*
         pd = new ProgressDialog(MainActivity.this);
         pd.setMessage("Processing...");
         pd.show();
-        webView.setWebViewClient(new MyWebViewClient());
-        webView.loadUrl(r_url);
-
+        webView.setWebViewClient(new MyWebViewClient());*/
 
     }
 
-    private class MyWebViewClient extends WebViewClient {
+   /* private class MyWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
@@ -56,6 +105,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-    }
+    }*/
 
 }
