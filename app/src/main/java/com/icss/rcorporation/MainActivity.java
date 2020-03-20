f6874eb9f20b6327367e8e9e24e65eb496db8ab3
LokeshInfo <lokesh.gadekar@infocentroidtech.com>
2020-03-20 15:30:47 +0530

    Updates 20 March 2020 :)

--- app/src/main/java/com/icss/rcorporation/MainActivity.java
+++ app/src/main/java/com/icss/rcorporation/MainActivity.java
@@ -1,15 +1,19 @@
 package com.icss.rcorporation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.GeolocationPermissions;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
@@ -17,25 +21,36 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
     ProgressDialog pd;
     private String r_url = "https://rcorporation.in/software/operator_login";
+    private TextView tv_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webview);
+        tv_location = findViewById(R.id.tv_location);
+       check_permission();
+/*
+        pd = new ProgressDialog(MainActivity.this);
+        pd.setMessage("Processing...");
+        pd.show();
+        webView.setWebViewClient(new MyWebViewClient());*/
 
+        tv_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             check_permission();     }});
 
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-        },0);
+    }
 
+    private void load_url(){
         WebSettings webSettings = webView.getSettings();
         webView.setWebViewClient(new WebViewClient());
 
@@ -84,16 +99,55 @@ public class MainActivity extends AppCompatActivity {
         });
 
         webView.loadUrl(r_url);
+    }
 
-/*
-        pd = new ProgressDialog(MainActivity.this);
-        pd.setMessage("Processing...");
-        pd.show();
-        webView.setWebViewClient(new MyWebViewClient());*/
+    private void check_permission(){
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+
+            webView.setVisibility(View.VISIBLE);
+            tv_location.setVisibility(View.GONE);
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            },0);
+
+        }
+
+        else{
+            webView.setVisibility(View.GONE);
+            tv_location.setVisibility(View.VISIBLE);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            },0);
+        }
 
     }
 
-   /* private class MyWebViewClient extends WebViewClient {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+
+            case 0:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    webView.setVisibility(View.VISIBLE);
+                    tv_location.setVisibility(View.GONE);
+                    load_url();   }
+                else{
+                    webView.setVisibility(View.GONE);
+                    tv_location.setVisibility(View.VISIBLE);
+                }}
+    }
+
+
+    /* private class MyWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
